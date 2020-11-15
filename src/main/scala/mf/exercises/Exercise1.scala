cedf8b523b336e1dea2a2c539d440a1182f00db6
pawelkaczor <newion@o2.pl>
2020-11-15 10:17:45 +0100

    exercise 1

--- src/main/scala/mf/exercises/Exercise1.scala
+++ src/main/scala/mf/exercises/Exercise1.scala
@@ -1,9 +1,10 @@
 package mf.exercises
 
-import scala.concurrent.{ExecutionContext, Future}
-import scala.util.Try
-import mf.models._
+import cats.implicits._
 import mf.http.Http
+import mf.models._
+
+import scala.concurrent.{ExecutionContext, Future}
 
 /**
   * Should fail if any call to Http.getBatch fails
@@ -11,8 +12,15 @@ import mf.http.Http
   * ParsedResponse.parser
   */
 class Exercise1(implicit ec: ExecutionContext) {
-  def requestBatch(requests: List[Request], batchSize: Int): Future[Either[ServiceError, List[ParsedResponse]]] = {
+  def requestBatch(
+      requests: List[Request],
+      batchSize: Int
+  ): Future[Either[ServiceError, List[ParsedResponse]]] = {
     val batches = requests.grouped(batchSize).toList.map(BatchRequest.apply)
-    ???
+    batches
+      .traverse(Http.getBatch)
+      .map(_.flatMap(r => r.responses).map(_.value).map(ParsedResponse.parser))
+      .map(_.traverse(_.toEither))
+      .map(_.leftMap(ex => ServiceError(ex.getMessage)))
   }
 }
